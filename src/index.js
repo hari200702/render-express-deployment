f35054a99301587ce8833122903530d3b2e9361e
Hari <harip@mquotient.net>
2025-07-02 20:39:23 +0530

    New One

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = express()
 
 
 app.use(cors({
-  origin: ['http://localhost:5173','https://canvocrm.netlify.app'],
+  origin: ['https://canvocrmmobile.netlify.app','https://canvocrm.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
