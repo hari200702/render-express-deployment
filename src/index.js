b9c2539e80064b9a8b8d14b04c726c8e1ab36b1b
Hari <harip@mquotient.net>
2025-07-02 18:37:53 +0530

    Final Build 6

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = express()
 
 
 app.use(cors({
-  origin: ['*'],
+  origin: ['http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
